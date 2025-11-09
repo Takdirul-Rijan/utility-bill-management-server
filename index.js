858809df2cb1156bf324477eb2a7f610179d4edf
Takdirul-Rijan <takdirul.rijan@gmail.com>
2025-11-10 03:44:35 +0600

    Added API endpoint for fetching single bill details by ID

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = 3000;
 
@@ -34,6 +34,13 @@ async function run() {
         .toArray();
       //   console.log(result);
 
+      res.send(result);
+    });
+
+    app.get("/bills/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await billsCollection.findOne(query);
       res.send(result);
     });
 
