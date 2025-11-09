347d2e4e342e72dd090aed9611fa0d880933c1c4
Takdirul-Rijan <takdirul.rijan@gmail.com>
2025-11-10 01:48:34 +0600

    Implemented bills API endpoint to fetch latest 6 bills from MongoDB

---
+++ index.js
@@ -0,0 +1,57 @@
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const app = express();
+const port = 3000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri =
+  "mongodb+srv://SmartBillHub-db:Sd8byiB4bgcgObEV@cluster0.qcldgif.mongodb.net/?appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+
+    const db = client.db("SmartBillHub-db");
+    const billsCollection = db.collection("bills");
+
+    app.get("/bills", async (req, res) => {
+      const result = await billsCollection
+        .find()
+        .sort({ date: -1 })
+        .limit(6)
+        .toArray();
+      //   console.log(result);
+
+      res.send(result);
+    });
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Smart server is running");
+});
+
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
