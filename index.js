0c49fc05d68df1a4573d6db73b445cfe120e7334
Takdirul-Rijan <takdirul.rijan@gmail.com>
2025-11-12 03:02:48 +0600

    Developed API endpoint to update user bill details by id

--- index.js
+++ index.js
@@ -72,6 +72,19 @@ async function run() {
       res.send(bills);
     });
 
+    // update a bill
+    app.put("/my-bills/:id", async (req, res) => {
+      const id = req.params.id;
+      const { amount, address, phone, date } = req.body;
+
+      const result = await myBillsCollection.updateOne(
+        { _id: new ObjectId(id) },
+        { $set: { amount, address, phone, date } }
+      );
+
+      res.send(result);
+    });
+
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
